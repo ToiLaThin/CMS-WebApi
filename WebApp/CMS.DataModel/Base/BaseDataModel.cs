fc87c6500ae4fadb4929ff71c6f9dda465dea6b4
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-06-07 18:16:05 +0700

    Add front end as a git submodule (but the folder is empty since i untracked its content) and added base model

---
+++ WebApp/CMS.DataModel/Base/BaseDataModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataModel.Base
+{
+    public interface IEntity<TType>
+    {
+        TType Id { get; set; }
+    }
+
+    public interface IBaseDataModel { 
+        DateTime CreateDate { get; set; }
+
+        short CreateBy { get; set; }
+
+        DateTime? ModifiedDate { get; set; }
+
+        short? ModifiedBy { get; set; }
+    }
+
+    public class BaseDataModel<TType> : IBaseDataModel, IEntity<TType>
+    {
+        public TType Id { get; set ; }
+        public DateTime CreateDate { get; set; }
+        public short CreateBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public short? ModifiedBy { get; set; }
+    }
+}
