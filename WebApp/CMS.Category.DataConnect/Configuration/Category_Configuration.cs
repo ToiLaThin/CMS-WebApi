52af49bba0d737b114e1368979988def72b99019
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-06-16 22:55:39 +0700

    fix migration post first then category so have order in making migration. Thats why i still not config automigrate db

--- WebApp/CMS.Category.DataConnect/Configuration/Category_Configuration.cs
+++ WebApp/CMS.Category.DataConnect/Configuration/Category_Configuration.cs
@@ -1,26 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace CMS.Category.DataConnect
-{
-    using CMS.DataModel;
-    internal partial class Category_Configuration : IEntityTypeConfiguration<Category>
-    {
-        public void Configure(EntityTypeBuilder<Category> builder)
-        {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasColumnName("CategoryId").UseIdentityColumn(0, 1);
-            builder.Property(x => x.Name).HasColumnName("CategoryName").HasMaxLength(500).IsRequired(true);
-            builder.Property(x => x.Description).HasColumnName("CategoryDescription").IsRequired(true);
-            builder.Property(o => o.CreateDate).HasColumnName("CategoryCreateDate").IsRequired(true).HasDefaultValueSql("getdate()");
-            builder.Property(o => o.CreateBy).HasColumnName("CategoryCreateBy").IsRequired(true).HasDefaultValue(0);
+//namespace CMS.Category.DataConnect
+//{
+//    using CMS.DataModel;
+//    internal partial class Category_Configuration : IEntityTypeConfiguration<Category>
+//    {
+//        public void Configure(EntityTypeBuilder<Category> builder)
+//        {
+//            builder.HasKey(x => x.Id);
+//            builder.Property(x => x.Id).HasColumnName("CategoryId").UseIdentityColumn(0, 1);
+//            builder.Property(x => x.Name).HasColumnName("CategoryName").HasMaxLength(500).IsRequired(true);
+//            builder.Property(x => x.Description).HasColumnName("CategoryDescription").IsRequired(true);
+//            builder.Property(o => o.CreateDate).HasColumnName("CategoryCreateDate").IsRequired(true).HasDefaultValueSql("getdate()");
+//            builder.Property(o => o.CreateBy).HasColumnName("CategoryCreateBy").IsRequired(true).HasDefaultValue(0);
 
-            //builder.HasMany(c => c.Posts).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId).IsRequired(true);
+//            //builder.HasMany(c => c.Posts).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId).IsRequired(true);
 
-            builder.HasData(
-                new { Id = -1, Name = "Default", Description = "Unknown" },
-                new { Id = -2, Name = "Sport", Description = "Physical Activitis and all kind of outdoor activities" }
-            );
-        }
-    }
-}
+//            builder.HasData(
+//                new { Id = -1, Name = "Default", Description = "Unknown" },
+//                new { Id = -2, Name = "Sport", Description = "Physical Activitis and all kind of outdoor activities" }
+//            );
+//        }
+//    }
+//}
