549e6bb9c02c75aa602cae34411af3fc0bcb2aab
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-07-31 14:20:03 +0700

    +config identity server mvc project which will communicate with auth
    redirect
    + try multiple startup project and only auth identity server and auth
      redirect
    + Changed port and ocelot path template

---
+++ WebApp/Auth.IdentityServer/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace IdentityServer.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IEnumerable<AuthenticationScheme> ExternalProviders { get; set; }
+    }
+}
