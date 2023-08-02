8c1ae566d0ebd1fb51396085afa1571af04fda5b
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2023-08-02 09:57:20 +0700

    modify frontend to login,set coookie then request token with that coookie, change backend cors in ocelot to include credentials(cookie), set cookie samesite attribute to none

--- WebApp/ApiGateWay.Ocelot/Program.cs
+++ WebApp/ApiGateWay.Ocelot/Program.cs
@@ -11,7 +11,12 @@ builder.Services.AddCors((setup) =>
 {
     setup.AddPolicy(corPolicyName, (options) =>
     {
-        options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+        //options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); this will cause cors error when request have with credentials
+        //ocelot is federation gateway which take and return to angular to it is set not Auth.Redirect
+        options.AllowAnyHeader()
+               .AllowAnyMethod()
+               .WithOrigins("http://localhost:4200") //will set AccessControlAllowOrigin header
+               .AllowCredentials();
     });
 });
 var app = builder.Build();
